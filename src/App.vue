e0b3d44269ea73cbd1252722ef5df7eb9a5aa52e
koreafood <koreafood@gmail.com>
2022-09-29 09:03:25 +0900

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@ export default defineComponent({
 
 <template>
   <div id="app">
-    <router-link to="/main">main</router-link>
-    <router-link to="/table">table</router-link>
-    <router-link to="/form">form</router-link>
-    <router-link to="/hello">hello</router-link>
+    <router-link to="/main" class="menu">main</router-link>
+    <router-link to="/table" class="menu">table</router-link>
+    <router-link to="/form" class="menu">form</router-link>
+    <router-link to="/hello" class="menu">hello</router-link>
     <router-view></router-view>
     <!--<img src="@/assets/logo.svg" alt="Vite logo" />-->
     <hr />
@@ -24,12 +24,15 @@ export default defineComponent({
 </template>
 
 <style>
+.menu {
+  @apply py-2 px-4 font-semibold rounded-lg shadow-md;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
